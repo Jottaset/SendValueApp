d5cc5cb39e21edccd4f092d43a73cb617a2084eb
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2019-03-11 22:03:43 -0400

    first commit

---
+++ SendValueApp/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SendValueApp
+{
+    public partial class MainPage : ContentPage
+    {
+        void Calcular(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new PageCalc(valor_01.Text, valor_02.Text));
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SendValueApp/PageCalc.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SendValueApp
+{
+    public partial class PageCalc : ContentPage
+    {
+        public PageCalc(string valor01, string valor02)
+        {
+            InitializeComponent();
+        }
+    }
+}
