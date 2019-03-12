820e6daca5f0af4e2f270cbfeecd5516525cc41b
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2019-03-12 19:46:22 -0400

    atualizando o commit

--- SendValueApp/PageCalc.xaml.cs
+++ SendValueApp/PageCalc.xaml.cs
@@ -9,7 +9,21 @@ namespace SendValueApp
     {
         public PageCalc(string valor01, string valor02)
         {
+            int a = Convert.ToInt32(valor01);
+            int b = Convert.ToInt32(valor02);
+
+            int c = a + b;
+
+
             InitializeComponent();
+            label_resultado.Text = c.ToString();
+
+        }
+
+        void Recalcular(object sender, System.EventArgs e)
+        {
+            Navigation.PopAsync();
+
         }
     }
 }
